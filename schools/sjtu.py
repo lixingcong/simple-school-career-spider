44ce5c8b1b8f2cc21156e4a56c09d587c87b9604
lixingcong <lixingcong@live.com>
2016-09-01 21:06:49 +0800

    SJTU is ok

--- schools/sjtu.py
+++ schools/sjtu.py
@@ -8,26 +8,69 @@ Created on 2016年9月1日
 import re
 import requests
 from schools.school_base import SCHOOL_BASE
+from bs4 import BeautifulSoup
 
 
 class SCHOOL_SJTU(SCHOOL_BASE): 
-	def __init__(self, title, host, url, encode=None, isFromLocal=False):
-		SCHOOL_BASE.__init__(self, title, host, url, encode=encode, isFromLocal=isFromLocal)
-		
+	def __init__(self, isFromLocal=False):
+		SCHOOL_BASE.__init__(self, u'上海交通大学', u'http://www.job.sjtu.edu.cn', u'/eweb/jygl/zpfw.so?modcode=jygl_zpxxck&subsyscode=zpfw&type=searchZpxx&zpxxType=new', isFromLocal=isFromLocal)
 	
-	
-	def find_parameters_and_get_real_link(self,input_string):
-		# input string should be like this:
-		# <a class="sy_a" href="javascript:void(0);" onclick="viewDwzpxx('PPaDGP38YQTscYHmhM3pWt', 'jygl_scfwyrdw')">康龙公司</a>
-		partern = re.compile("viewDwzpxx\(.*\)\">")
-		result = partern.findall(input_string)
-		if result:
-			result = result[0].split('\'')
-			for i in result:
-				print i
-		else:
-			print "no found"
+	def recursive_get_each_entry(self):
+		if self.content_original:
+			res = BeautifulSoup(self.content_original, "html.parser")
+			lis = res.select('.z_newsl li')
 
+			for li in lis[1:]:
+				list_one = []
+				divs = list(li.find_all('div'))
+				# company name + date
+				list_one.append(divs[2].string[5:] + divs[0].a.string)
+				# link
+				fake_link = str(divs[1].a['onclick']).strip()
+				list_one.append(self.get_real_link(fake_link))
+				# job name
+				list_one.append(divs[1].a.string.strip())
+
+				# if company exists then add to dict
+				if list_one[0] in self.dict_all.iterkeys():
+					self.dict_all[list_one[0]].append(list_one)
+				else:
+					list_to_insert = []
+					list_to_insert.append(list_one)
+					self.dict_all[list_one[0]] = list_to_insert
+	
+	def convert_to_table(self):
+		self.content += (u'<h3>' + self.title + u'</h3>')
+		if self.dict_all == {}:
+			self.content += u'<p>抓取内容为空</p>'
+		else:
+			self.content += u'<table>'
+			self.content += u'<tr><th>公司 发布日期</th><th>职位</th></tr>'
+			for list1 in reversed(sorted(self.dict_all.iterkeys())):
+				len1 = len(self.dict_all[list1])
+				is_firstline = True
+				self.content += u'<tr><th rowspan="' + str(len1) + u'">' + list1[5:] + u' ' + list1[:5] + u'</th>'
+				for i in self.dict_all[list1]:
+					if is_firstline == False:
+						self.content += u'<tr>'
+					self.content += u'<th><a href="' + self.host + i[1] + u'">' + i[2] + u'</a></tr>'
+					is_firstline = False
+			self.content += u'</table>'
+	
+	def get_real_link(self, input_string):
+		# input string should be like this:
+		# viewZpxx('PPaDGP38YQTscYHmhM3pWt', 'jygl_scfwyrdw')
+		splited_str = input_string.split('\'')
+		param_1 = splited_str[1]
+		param_2 = splited_str[3]
+		return u'/eweb/jygl/zpfw.so?modcode=' + param_2 + u'&subsyscode=zpfw&type=viewZpxx&id=' + param_1
+			
+
+		
 if __name__ == '__main__':
-	obj=SCHOOL_SJTU(u'上海交通大学',u'http://www.job.sjtu.edu.cn',u'/eweb/jygl/zpfw.so?modcode=jygl_zpxxck&subsyscode=zpfw&type=searchZpxx&zpxxType=new',isFromLocal=True)
+	obj = SCHOOL_SJTU(isFromLocal=True)
+	content = u'<html><head><meta charset="utf-8"><style>table, th, td { border: 1px solid #99cccc; text-align: left;}</style></head><body><h2>未来七日宣讲会</h2>'
+	content += obj.get_HTML()
+	content += u'<p>由<a href="http://lixingcong.github.io">Lixingcong</a>使用python强力驱动</p></body></html>' 
+	print content	
 	
