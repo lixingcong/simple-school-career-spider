4e2bb85ea3f450ab88b550f268561817cca02bd3
lixingcong <lixingcong@live.com>
2016-09-01 13:56:04 +0800

    cumt is ok

---
+++ schools/cumt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2016年9月1日
+@author: li
+'''
+from schools.template_91job_gov_cn import SCHOOL_91JOB_GOV_CN
+
+class SCHOOL_CUMT(SCHOOL_91JOB_GOV_CN):
+	pass
+
+if __name__ == '__main__':
+	c = SCHOOL_CUMT(u"中国矿业大学", u'http://jyzd.cumt.edu.cn', u'/teachin?time=14', isFromLocal=False)		
+	content = u'<html><head><meta charset="utf-8"><style>table, th, td { border: 1px solid #99cccc; text-align: left;}</style></head><body><h2>未来七日宣讲会</h2>'
+	content += c.get_HTML()
+	content += u'<p>由<a href="http://lixingcong.github.io">Lixingcong</a>使用python强力驱动</p></body></html>' 
+	print content
